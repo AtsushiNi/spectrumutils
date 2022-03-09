b0e4f0a0d9d540c3f6584423dca4c9b947210da2
AtsushiNi <atsushi9731@gmail.com>
2022-03-10 01:47:33 +0900

    fix

--- src/spectrumutils/fulcher.py
+++ src/spectrumutils/fulcher.py
@@ -2,7 +2,14 @@ import numpy as np
 from importlib_resources import files
 
 def read():
+    """
     franck_condon = files('src.spectrumutils.data.fulcher').joinpath('franck_condon_factor.txt').read_text()
     print(franck_condon)
     franck_condon = files('src.spectrumutils.data.fulcher').joinpath('franck_condon_factor_d-to-a.csv').read_text()
     print(franck_condon)
+    """
+    franck_condon = files('src.spectrumutils.data.fulcher').joinpath('franck_condon_factor.txt')
+    with as_file(franck_condon) as f:
+        r = np.loadtxt(f)
+    print(r)
+
